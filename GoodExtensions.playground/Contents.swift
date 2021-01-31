1023813aa30e7cbaeab7c04adc6348b567bfccde
Noshaid Ali <noshaid@gmail.com>
2021-01-31 18:51:57 +0500

    add another shadow method

--- GoodExtensions.playground/Contents.swift
+++ GoodExtensions.playground/Contents.swift
@@ -9,6 +9,20 @@ extension UIView {
         self.layer.shadowRadius = 5
     }
     
+    func addShadow(
+       cornerRadius: CGFloat = 16,
+       shadowColor: UIColor = UIColor(white: 0.0, alpha: 0.5),
+       shadowOffset: CGSize = CGSize(width: 0.0, height: 3.0),
+       shadowOpacity: Float = 0.3,
+       shadowRadius: CGFloat = 5) {
+      
+          layer.cornerRadius = cornerRadius
+          layer.shadowColor = shadowColor.cgColor
+          layer.shadowOffset = shadowOffset
+          layer.shadowOpacity = shadowOpacity
+          layer.shadowRadius = shadowRadius
+    }
+    
     func shake() {
         let shake = CABasicAnimation(keyPath: "position")
         shake.duration = 0.05
@@ -25,6 +39,7 @@ extension UIView {
 }
 let view = UIView()
 view.dropShadow()
+view.addShadow()
 view.shake()
 
 
